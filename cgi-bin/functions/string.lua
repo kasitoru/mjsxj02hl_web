25085b546a6aa3cc7b1c568692c81b4d391a606f
Sergey Avdeev <avdeevsv91@gmail.com>
2021-06-09 10:30:22 +0300

    added is_string() function

--- cgi-bin/functions/string.lua
+++ cgi-bin/functions/string.lua
@@ -3,6 +3,11 @@ local functions = {
 }
 package.loaded[...] = functions.string
 
+-- Checks if the passed argument is a string
+functions.string.is_string = function(str)
+    return (type(str) == "string")
+end
+
 -- Check if the string is empty
 functions.string.is_empty = function(str)
     return (str == nil or str == "")
@@ -10,7 +15,7 @@ end
 
 -- Escapes special pattern characters
 functions.string.escape_pattern = function(str)
-    if str then
+    if functions.string.is_string(str) then
         local matches = {
             ["^"] = "%^",
             ["$"] = "%$",
@@ -33,7 +38,7 @@ end
 
 -- Count the number of substring occurrences
 functions.string.substr_count = function(str, sub)
-    if str and sub then
+    if functions.string.is_string(str) and functions.string.is_string(sub) then
         sub = functions.string.escape_pattern(sub)
         local _, count = string.gsub(str, sub, "")
         return count
@@ -44,7 +49,7 @@ end
 -- Split the string by separator into table
 functions.string.split = function(str, sep)
     local data = {}
-    if str and sep then
+    if functions.string.is_string(str) and functions.string.is_string(sep) then
         sep = functions.string.escape_pattern(sep)
         for part in string.gmatch(str, "([^" .. sep .. "]+)") do
             table.insert(data, part)
@@ -55,7 +60,7 @@ end
 
 -- Left trim string
 functions.string.ltrim = function(str, symbol)
-    if str then
+    if functions.string.is_string(str) then
         if functions.string.is_empty(symbol) then
             symbol = " "
         end
@@ -67,7 +72,7 @@ end
 
 -- Right trim string
 functions.string.rtrim = function(str, symbol)
-    if str then
+    if functions.string.is_string(str) then
         if functions.string.is_empty(symbol) then
             symbol = " "
         end
@@ -79,7 +84,7 @@ end
 
 -- Trim string
 functions.string.trim = function(str, symbol)
-    if str then
+    if functions.string.is_string(str) then
         str = functions.string.ltrim(str, symbol)
         str = functions.string.rtrim(str, symbol)
         return str
@@ -89,7 +94,7 @@ end
 
 -- Uppercase first letter
 functions.string.first_letter = function(str)
-    if str then
+    if functions.string.is_string(str) then
         return string.gsub(str, "^%l", string.upper)
     end
     return ""
@@ -97,7 +102,7 @@ end
 
 -- Replace all occurrences of the search string with the replacement string
 functions.string.replace = function(search, replace, subject)
-    if search and subject then
+    if functions.string.is_string(search) and functions.string.is_string(replace) and functions.string.is_string(subject) then
         search = functions.string.escape_pattern(search)
         replace = functions.string.escape_pattern(replace)
         subject = string.gsub(subject, search, replace)
@@ -107,7 +112,7 @@ end
 
 -- Check if the string start width substring
 functions.string.start_width = function(str, start)
-    if str and start then
+    if functions.string.is_string(str) and functions.string.is_string(start) then
         return (string.sub(str, 1, string.len(start)) == start)
     end
     return false
@@ -115,7 +120,7 @@ end
 
 -- Check if the string end width substring
 functions.string.end_width = function(str, ends)
-    if str and ends then
+    if functions.string.is_string(str) and functions.string.is_string(ends) then
         return (string.sub(str, -string.len(ends)) == ends)
     end
     return false
