0cdf37f39bf984092de7ea0c02e68201991f88c1
Sergey Avdeev <avdeevsv91@gmail.com>
2021-11-29 00:30:24 +0300

    Update app.lua

--- www/cgi-bin/functions/app.lua
+++ www/cgi-bin/functions/app.lua
@@ -93,7 +93,7 @@ functions.app.default_settings = function()
             qos                     = 1,           -- Quality of Service (0, 1 or 2)
             retain                  = false,       -- Retained messages
             reconnection_interval   = 60,          -- Reconnection interval (in seconds)
-            periodical_interval     = 300,         -- Interval of periodic message (in seconds)
+            periodical_interval     = 60,          -- Interval of periodic message (in seconds)
         },
         night = {
             mode                    = 2,           -- Night mode (0 = off, 1 = on, 2 = auto)
